b0e3474d772060d706d9c0d3ff5107b21546d741
sheezu900 <shahzamanlive2@gmail.com>
2023-12-09 23:01:12 +0500

    remove commented lines

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,4 @@
-// const path    = require("path")
-// const webpack = require("webpack")
 
-// module.exports = {
-//   mode: "production",
-//   devtool: "source-map",
-//   entry: {
-//     application: "./app/javascript/application.js"
-//   },
-//   output: {
-//     filename: "[name].js",
-//     sourceMapFilename: "[file].map",
-//     path: path.resolve(__dirname, "app/assets/builds"),
-//   },
-//   plugins: [
-//     new webpack.optimize.LimitChunkCountPlugin({
-//       maxChunks: 1
-//     })
-//   ]
-// }
 const path = require('path');
 const webpack = require('webpack');
 
